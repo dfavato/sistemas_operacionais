2e0d89a5b3dca2483a4d27ec9a1ab777db539bbf
dfavato <dfavato@gmail.com>
2018-06-15 15:41:34 -0300

    Lendo corretamente inodes do segundo grupo

--- src/ext2.c
+++ src/ext2.c
@@ -5,6 +5,7 @@
 #include <linux/magic.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <time.h>
 #include "ext2.h"
 
 // Variável global para guardar o storage_device que vai ser único
@@ -23,11 +24,13 @@ struct ext2_inode* get_root_directory(struct ext2_super_block super);
 int roundup(double);
 void print_inode_info(struct ext2_inode inode);
 void ls(struct ext2_inode inode, struct ext2_super_block super);
+struct ext2_inode* read_inode(__le32 inode_nr, struct ext2_super_block super);
+struct ext2_inode* get_inode_by_name(struct ext2_inode root, char *name, struct ext2_super_block super);
 
 int main(int argc, char *argv[]) {
 	char imgpath[255];
 	struct ext2_super_block *super;
-	struct ext2_inode *curdir;
+	struct ext2_inode *curdir, *dir;
 
 	if(argc <= 1) {
 		fprintf(stderr, "Informe um arquivo de imagem.\n");
@@ -47,12 +50,14 @@ int main(int argc, char *argv[]) {
 	}
 	sb(*super);
 	curdir = get_root_directory(*super);
-	print_inode_info(*curdir);
 	ls(*curdir, *super);
+	dir = get_inode_by_name(*curdir, "documents", *super);
+	ls(*dir, *super);
 
 	// Free resources
 	free(super);
 	free(curdir);
+	free(dir);
 	close(storage_device);
 	return EXIT_SUCCESS;
 }
@@ -84,7 +89,11 @@ void sb(struct ext2_super_block super) {
 struct ext2_group_desc* get_group_desc(int group, struct ext2_super_block super) {
 	// TODO apenas o primeiro grupo é retornado... usar group para calcular o offset
 	struct ext2_group_desc *gd = (struct ext2_group_desc*)malloc(sizeof(struct ext2_group_desc));
-	lseek(storage_device, BASE_OFFSET + block_size(super), SEEK_SET);
+	lseek(
+		storage_device,
+		BASE_OFFSET + block_size(super) + (group)*super.s_blocks_per_group*block_size(super),
+		SEEK_SET
+	);
 	read(storage_device, gd, sizeof(*gd));
 	return gd;
 }
@@ -124,16 +133,52 @@ int block_offset(int block, struct ext2_super_block super) {
 
 struct ext2_inode* get_root_directory(struct ext2_super_block super) {
 	// O diretório root fica no inode 2 https://wiki.osdev.org/Ext2
-	struct ext2_group_desc *gd = get_group_desc(inode_group(EXT2_ROOT_INO, super), super);
+	return read_inode(EXT2_ROOT_INO, super);
+}
+
+struct ext2_inode* read_inode(__le32 inode_nr, struct ext2_super_block super) {
+	int group = inode_group(inode_nr, super);
+	int index = inode_index(inode_nr, super);
+	struct ext2_group_desc *gd = get_group_desc(group, super);
+	long int offset = block_offset(
+		gd->bg_inode_table + group*super.s_blocks_per_group, super
+	) + index * sizeof(struct ext2_inode);
 	struct ext2_inode *inode = (struct ext2_inode*) malloc(sizeof(struct ext2_inode));
-	lseek(
-		storage_device,
-		block_offset(gd->bg_inode_table, super) + (EXT2_ROOT_INO - 1) * sizeof(struct ext2_inode),
-		SEEK_SET
-	);
+	lseek(storage_device, offset, SEEK_SET);
 	read(storage_device, inode, sizeof(*inode));
 	free(gd);
 	return inode;
+}
+
+struct ext2_inode* get_inode_by_name(struct ext2_inode root, char *name, struct ext2_super_block super) {
+	struct ext2_inode *inode = NULL;
+	if(!S_ISDIR(root.i_mode)) {
+		fprintf(stderr, "Diretório root inválido.\n");
+		return inode;
+	}
+	struct ext2_dir_entry_2 *entry;
+	unsigned int bytes_read = 0;
+	int bs = block_size(super);
+	void *block = malloc(bs);
+	char file_name[EXT2_NAME_LEN+1];
+	int inode_nr;
+
+	lseek(storage_device, block_offset(root.i_block[0], super), SEEK_SET);
+	read(storage_device, block, bs);
+	entry = (struct ext2_dir_entry_2*)block;
+	while(bytes_read < root.i_size && entry->inode) {
+		memcpy(file_name, entry->name, entry->name_len);
+		file_name[entry->name_len] = '\0';
+		if(!strcmp(file_name, name)) {
+			// inode encontrado
+			inode_nr = entry->inode;
+			printf("Inode %s encontrado: %d\n", name, entry->inode);
+			free(block);
+			return read_inode(inode_nr, super);
+		}
+		bytes_read += entry->rec_len;
+		entry = (void*) entry + entry->rec_len;
+	}
 }
 
 int roundup(double x) {
@@ -144,43 +189,78 @@ int roundup(double x) {
 }
 
 void print_inode_info(struct ext2_inode inode) {
+	char time_string[100];
+	time_t date;
+	char *format = "%Y-%m-%d %H:%M:%S %Z00";
 	printf("File mode: %d\n", inode.i_mode);
 	if(S_ISDIR(inode.i_mode)) {
-		printf("DIretório!\n");
+		printf("Diretório!\n");
 	}
+	printf("Size: %d\t", inode.i_size);
+	printf("Blocks: %d\t", inode.i_blocks);
+	printf("IO Block: \t");
+	if(S_ISDIR(inode.i_mode)) {
+		printf("directory\n");
+	} else if (S_ISREG(inode.i_mode)) {
+		printf("regular file\n");
+	} else if (S_ISCHR(inode.i_mode)) {
+		printf("character device\n");
+	} else if (S_ISBLK(inode.i_mode)) {
+		printf("block device\n");
+	} else if (S_ISFIFO(inode.i_mode)) {
+		printf("fifo\n");
+	} else if (S_ISSOCK(inode.i_mode)) {
+		printf("socket\n");
+	} else if (S_ISLNK(inode.i_mode)) {
+		printf("symbolic link\n");
+	} else {
+		printf("Unkown tipe\n");
+	}
+
 	printf("Owner: %d\n", inode.i_uid);
-	printf("Size: %d\n", inode.i_size);
-	printf("Access time: %d\n", inode.i_atime);
-	printf("Creation time: %d\n", inode.i_ctime);
-	printf("Modification time: %d\n", inode.i_mtime);
-	printf("Deletion time: %d\n", inode.i_dtime);
+
+	date = inode.i_atime;
+	strftime(time_string, sizeof(time_string), format, localtime(&date));
+	printf("Access time: %s\n", time_string);
+	
+	date = inode.i_ctime;
+	strftime(time_string, sizeof(time_string), format, localtime(&date));
+	printf("Creation time: %s\n", time_string);
+	
+	date = inode.i_mtime;
+	strftime(time_string, sizeof(time_string), format, localtime(&date));
+	printf("Modification time: %s\n", time_string);
+	
+	date = inode.i_dtime;
+	strftime(time_string, sizeof(time_string), format, localtime(&date));
+	printf("Deletion time: %s\n", time_string);
+	
 	printf("Group: %d\n", inode.i_gid);
 	printf("Links: %d\n", inode.i_links_count);
-	printf("Blocks: %d\n", inode.i_blocks);
 	printf("Flags: %d\n", inode.i_flags);
 }
 
 void ls(struct ext2_inode inode, struct ext2_super_block super) {
 	if(!S_ISDIR(inode.i_mode)) {
-		fprintf(stderr, "Diretório inválido.\n");
+		fprintf(stderr, "Diretório inválido, i_mode: %d\n", inode.i_mode);
+		print_inode_info(inode);
 		return;
 	}
 	struct ext2_dir_entry_2 *entry;
 	unsigned int bytes_read = 0;
 	int bs = block_size(super);
 	void *block = malloc(bs);
+	char file_name[EXT2_NAME_LEN+1];
 
 	lseek(storage_device, block_offset(inode.i_block[0], super), SEEK_SET);
 	read(storage_device, block, bs);
-	printf("LS\n");
 	entry = (struct ext2_dir_entry_2 *)block;
 	while(bytes_read < inode.i_size && entry->inode) {
-		char file_name[EXT2_NAME_LEN+1];
 		memcpy(file_name, entry->name, entry->name_len);
 		file_name[entry->name_len] = '\0';
 		printf("%s\t", file_name);
-		entry = (void*) entry +  entry->rec_len;
 		bytes_read += entry->rec_len;
+		entry = (void*) entry +  entry->rec_len;
 	}
 	printf("\n");
 	free(block);
